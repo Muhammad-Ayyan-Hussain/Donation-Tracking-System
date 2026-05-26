f3546608dc8766954717cfb1f0f5ed4b2265ca69
Muhammad Ayyan Hussain <muhammadayyanhussain45@gmail.com>
2026-05-26 17:56:46 +0500

    Delete Donation Tracking Database directory

--- Donation Tracking Database/DonationSystem/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector(".sidebar");
-
-if (sidebar) {
-    sidebar.scrollTop = localStorage.getItem("sidebarScroll") || 0;
-
-    sidebar.addEventListener("scroll", function () {
-        localStorage.setItem("sidebarScroll", sidebar.scrollTop);
-    });
-}
